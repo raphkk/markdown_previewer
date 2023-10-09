ba593836ea50ccf38a96a25f20c3764042b2b363
Raphael kiteme <raphael.kituku@gmail.com>
2023-10-09 12:00:08 +0400

    created a function in my js file

--- script.js
+++ script.js
@@ -13,3 +13,10 @@
       Object.defineProperty(target, descriptor.key, descriptor);
     }
   }
+
+
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
