13e6f73a7b38e1fd3693f66b544ffd1bb8f96b93
Raphael kiteme <raphael.kituku@gmail.com>
2023-10-10 12:35:56 +0400

    provided js code

--- script.js
+++ script.js
@@ -20,3 +20,13 @@
     if (staticProps) _defineProperties(Constructor, staticProps);
     return Constructor;
   }
+
+
+  function _unsupportedIterableToArray(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(o);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+  }
