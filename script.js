8495270c253bb05b0b4ff74290fc07df759ab3ed
Raphael kiteme <raphael.kituku@gmail.com>
2023-10-12 22:47:31 +0400

    updated js file

--- script.js
+++ script.js
@@ -38,3 +38,19 @@
 
     return arr2;
   }
+
+   function _createForOfIteratorHelperLoose(o) {
+    var i = 0;
+
+    if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) {
+      if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) return function () {
+        if (i >= o.length) return {
+          done: true
+        };
+        return {
+          done: false,
+          value: o[i++]
+        };
+      };
+      throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+    }
