001a2627fcb4a16853b13bffc582df651d8db153
Raphael kiteme <raphael.kituku@gmail.com>
2023-10-22 20:06:55 +0400

    updated js file

--- script.js
+++ script.js
@@ -134,3 +134,15 @@
 
     return html;
   }
+
+    var unescapeTest = /&(#(?:\d+)|(?:#x[0-9A-Fa-f]+)|(?:\w+));?/ig;
+
+  function unescape(html) {
+    // explicitly match decimal, hex, and named HTML entities
+    return html.replace(unescapeTest, function (_, n) {
+      n = n.toLowerCase();
+      if (n === 'colon') return ':';
+
+      if (n.charAt(0) === '#') {
+        return n.charAt(1) === 'x' ? String.fromCharCode(parseInt(n.substring(2), 16)) : String.fromCharCode(+n.substring(1));
+      }
