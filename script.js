02a18a2fe1c8a222a43dbc9ba9d9fe0f59f272cb
Raphael kiteme <raphael.kituku@gmail.com>
2023-10-26 16:38:30 +0400

    updated js file

--- script.js
+++ script.js
@@ -177,3 +177,27 @@
   function cleanUrl(sanitize, base, href) {
     if (sanitize) {
       var prot;
+
+            try {
+        prot = decodeURIComponent(unescape(href)).replace(nonWordAndColonTest, '').toLowerCase();
+      } catch (e) {
+        return null;
+      }
+
+      if (prot.indexOf('javascript:') === 0 || prot.indexOf('vbscript:') === 0 || prot.indexOf('data:') === 0) {
+        return null;
+      }
+    }
+
+    if (base && !originIndependentUrl.test(href)) {
+      href = resolveUrl(base, href);
+    }
+
+    try {
+      href = encodeURI(href).replace(/%25/g, '%');
+    } catch (e) {
+      return null;
+    }
+
+    return href;
+  }
