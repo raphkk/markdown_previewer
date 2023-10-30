136b39bf878c5cbf35a9f8f612ae1deab6148275
Raphael kiteme <raphael.kituku@gmail.com>
2023-10-30 09:41:07 +0400

    updated js file

--- script.js
+++ script.js
@@ -239,3 +239,25 @@
       return base + href;
     }
   }
+
+  var noopTest = {
+    exec: function noopTest() {}
+  };
+
+  function merge(obj) {
+    var i = 1,
+        target,
+        key;
+
+    for (; i < arguments.length; i++) {
+      target = arguments[i];
+
+      for (key in target) {
+        if (Object.prototype.hasOwnProperty.call(target, key)) {
+          obj[key] = target[key];
+        }
+      }
+    }
+
+    return obj;
+  }
