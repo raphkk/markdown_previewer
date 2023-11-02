2086f3ba7ca6ae709e279b088f9441c7270fcef0
Raphael kiteme <raphael.kituku@gmail.com>
2023-11-03 01:37:00 +0400

    updated js file

--- script.js
+++ script.js
@@ -327,3 +327,30 @@
 
     return str.substr(0, l - suffLen);
   }
+
+
+  function findClosingBracket(str, b) {
+    if (str.indexOf(b[1]) === -1) {
+      return -1;
+    }
+
+    var l = str.length;
+    var level = 0,
+        i = 0;
+
+    for (; i < l; i++) {
+      if (str[i] === '\\') {
+        i++;
+      } else if (str[i] === b[0]) {
+        level++;
+      } else if (str[i] === b[1]) {
+        level--;
+
+        if (level < 0) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
+  }
