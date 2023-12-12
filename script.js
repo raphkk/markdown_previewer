ec84531bb1e3a3082f127d1227bfb8a2639b05da
Raphael kiteme <raphael.kituku@gmail.com>
2023-12-12 11:15:07 +0400

    updated js file

--- script.js
+++ script.js
@@ -1192,3 +1192,16 @@
   /**
    * smartypants text replacement
    */
+  function smartypants(text) {
+    return text // em-dashes
+    .replace(/---/g, "\u2014") // en-dashes
+    .replace(/--/g, "\u2013") // opening singles
+    .replace(/(^|[-\u2014/(\[{"\s])'/g, "$1\u2018") // closing singles & apostrophes
+    .replace(/'/g, "\u2019") // opening doubles
+    .replace(/(^|[-\u2014/(\[{\u2018\s])"/g, "$1\u201C") // closing doubles
+    .replace(/"/g, "\u201D") // ellipses
+    .replace(/\.{3}/g, "\u2026");
+  }
+  /**
+   * mangle email addresses
+   */
