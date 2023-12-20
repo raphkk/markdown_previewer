b8b946ef928645457b8e24ebd67b003ac591f15c
Raphael kiteme <raphael.kituku@gmail.com>
2023-12-20 11:27:16 +0300

    updated js file

--- script.js
+++ script.js
@@ -1306,3 +1306,18 @@
 
           continue;
         } // code
+
+
+        if (token = this.tokenizer.code(src, tokens)) {
+          src = src.substring(token.raw.length);
+
+          if (token.type) {
+            tokens.push(token);
+          } else {
+            lastToken = tokens[tokens.length - 1];
+            lastToken.raw += '\n' + token.raw;
+            lastToken.text += '\n' + token.text;
+          }
+
+          continue;
+        } // fences
